ad5b408d0e2eedceebc5cc6e47dc8ab8b0a80244
Jessica Andersson <ja@speedledger.se>
2015-08-11 11:35:40 +0200

    Select preview urls and create game object

---
+++ server/game.js
@@ -0,0 +1,26 @@
+Meteor.methods({
+    startGame: function(gameName) {
+        var cityName = "New York";
+        var city = getCity(cityName);
+        var artists = getArtistsForCity(cityName).splice(0,10);
+        artists = appendSongDataToArtists(artists);
+
+        currentSongURL = [];
+        var songPreviewURLs = [];
+        artists.forEach(function(entry) {
+            songPreviewURLs.push(entry.songs[0].preview_url);
+        });
+        
+        console.log(songPreviewURLs);
+        Games.insert({currentSong: 0, previews: songPreviewURLs});
+    }
+});
+
+function startPlayList(gameName, songs, i){
+        if(i < 10){
+          Games.update(gameName, {
+              $set: {currentSong: songs[i]}
+          });
+          Meteor.setTimeout(function(){startPlayList(gameName, songs, i + 1);}, 30000);
+        }
+}
