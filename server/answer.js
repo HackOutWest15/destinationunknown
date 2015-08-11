091004b01387854a1003a4c4c2b44e8bd2746136
Jessica Andersson <ja@speedledger.se>
2015-08-11 15:21:17 +0200

    To lower case and remove trailing white spaces

--- server/answer.js
+++ server/answer.js
@@ -1,5 +1,7 @@
 Meteor.methods({
     checkAnswer: function(gameId, answerToCheck) {
+        answerToCheck = answerToCheck.toLowerCase();
+        answerToCheck = answerToCheck.trim();
         return Answers.find({gameId: gameId, city: answerToCheck}).count() > 0;
     }
 });
