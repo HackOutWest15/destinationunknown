db518bc1fbd16a5826c99d44813436f511c4c1e4
Eng <c@rlengstrom.se>
2015-08-15 14:35:29 +0200

    Reversed logic in lobby.js for readability (and perhaps to fix a bug?)

--- server/lobby.js
+++ server/lobby.js
@@ -4,17 +4,16 @@ Meteor.methods({
         	{stoppedAt:0, score:0, guess:"No guess"}]};
             
         var game = Games.findOne({_id:gameId});
-        if(!game.players.length>0){            
+        if(game.players.length == 0){            
             Games.update( {_id:gameId}, { $push: {players: player } });
         }
-        else if(!game.players[0].playerId == player.playerId){
+        else if(game.players[0].playerId != player.playerId){
             Games.update( {_id:gameId}, { $push: {players: player } });
             var gameReady = Games.find( { _id:gameId, players: {$size: 2} } ).count();
             if(gameReady > 0) {
                 startGame(gameId, 0);
             }     
         }
-          
     }
 });
 
